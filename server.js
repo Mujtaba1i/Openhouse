af79934ca31caa9eabc1b0b81435e47c2a23b07d
Mujtaba1i <unmujt@gmail.com>
2025-12-10 09:31:42 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,90 @@
+// imports ===========================================================================================
+
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const session = require('express-session')
+const {MongoStore} = require("connect-mongo")
+const isSignedIn = require("./middleware/isSignedIn")
+const passUserToView = require("./middleware/passUserToView.js")
+const path = require('path')
+const app = express()
+const port = process.env.PORT ? process.env.PORT : "4000" 
+
+// Connecting to DB ==================================================================================
+
+require('./config/database.js');
+
+// controller(s) =====================================================================================
+
+const authCtrl = require("./controllers/auth")
+
+// cookies ===========================================================================================
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+                mongoUrl: process.env.MONGODB_URI
+            })
+        })
+)
+
+// Style Sheet =======================================================================================
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// middleware ========================================================================================
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
+
+// Routes ============================================================================================
+
+// Public Routes =====================================================================================
+
+// passing the user
+app.use(passUserToView)
+
+// homepage
+app.get("/", async (req, res) => {
+    res.render('index.ejs')
+})
+
+// auth Routes
+app.use('/auth' , authCtrl)
+
+
+// Protected Routes ==================================================================================
+app.use(isSignedIn)
+app.get('/vip-lounge', async(req,res)=>{
+    res.send('VIP PAGE')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listening =========================================================================================
+app.listen(port, () => console.log(`listening on port ${port}`))
